8188eca3b5b9e30b3c550dc1118ee1d3688606ba
Georgyyx <julyfortune101@gmail.com>
2021-10-17 18:56:38 +0300

    more readable check + tested

--- task_1/mul_st.sol
+++ task_1/mul_st.sol
@@ -4,7 +4,7 @@ pragma AbiHeader expire;
 contract ProductStore {
 
     uint public product = 1;
-    uint multiplier;
+    //uint multiplier;
 
     constructor() public {
         require(tvm.pubkey() != 0, 101);
@@ -13,12 +13,19 @@ contract ProductStore {
     }
 
 //variable should be in range from 1 to 10
-    modifier check_tenity(uint _multiplier) {
-        require(_multiplier > 0 && _multiplier < 11, 110, "multiplier not in rel");
-        _;
-    }
+    // modifier check_tenity(uint _multiplier) {
+    //     require(_multiplier > 0 && _multiplier < 11, 110, "multiplier not in rel");
+    //     tvm.accept();
+    //     _;
+    // }
 
-    function mul() public check_tenity(multiplier) {
-        product *= multiplier;
+    // function mul() public check_tenity(_multiplier) {
+    //     product *= _multiplier;
+    // }
+
+    function mul_simplier(uint8 _multer) public {
+        require(_multer >= 1 && _multer <= 10, 111, "in range from 1 to 10, please");
+        product *= _multer;
+        tvm.accept();
     }
 }
