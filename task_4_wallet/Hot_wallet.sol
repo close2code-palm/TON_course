a60be7b33bcf1bfa3e401d807a70a67bbf35fb52
Georgyyx <julyfortune101@gmail.com>
2021-10-29 01:21:04 +0300

    fixed import, added abi and tvc files

--- task_4_wallet/Hot_wallet.sol
+++ task_4_wallet/Hot_wallet.sol
@@ -1,15 +1,15 @@
 pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
-import 'https://github.com/tonlabs/samples/blob/master/solidity/10_Wallet.sol' as dWallt;
+import './exampleWallet.sol';
 
-contract HotWallet is dWallt {
+contract HotWallet is Wallet {
 
     function denom(address dst) public checkOwnerAndAccept {
         uint16 flag = 160;
         dst.transfer(flag);
     }
 
-    function send_wo_comsn(address dst, uint quantity) public checkOwnerAndAccept {
+    function send_wo_comsn(address dst, uint128 quantity) public checkOwnerAndAccept {
         uint16 flag = 0;
         dst.transfer(quantity, true, flag);
     }
