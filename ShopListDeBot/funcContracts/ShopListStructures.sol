3c51551d0b459ad2d2ee399bdcf4fb498fa68d3c
Georgyyx <julyfortune101@gmail.com>
2021-11-08 16:46:34 +0300

    seprate .sol with item and summary structs

---
+++ ShopListDeBot/funcContracts/ShopListStructures.sol
@@ -0,0 +1,14 @@
+struct ToBuy {
+        uint32 id;
+        string naming;
+        uint16 amount;
+        uint32 addedAt;
+        bool bought;
+        uint128 price;
+    }
+
+struct BuysSummary {
+    uint32 paid;
+    uint32 notPaid;
+    uint totalPrice;    
+}
